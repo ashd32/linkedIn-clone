4e1f2ebf6919bccba46d326be8fa6decb1eb268e
MaksimLion <maxim226356@mail.ru>
2019-07-30 13:18:51 +0300

    deleted extra folder

--- loger/app/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from sanic import Sanic
-from .controllers.logs import LogController
-
-
-def create_app():
-    app = Sanic()
-    app.add_route(LogController.as_view(), '/api/logs')
-    app.go_fast(debug=True, workers=os.cpu_count())
--- loger/app/controllers/logs.py
+++
@@ -1,8 +0,0 @@
-from sanic.response import json
-from sanic.views import HTTPMethodView
-
-
-class LogController(HTTPMethodView):
-    
-    async def post(self, request):
-        pass
