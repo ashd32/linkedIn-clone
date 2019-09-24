83a96ebc823c7f39260d8fac18f5e39c0fa2020b
MaksimLion <maxim226356@mail.ru>
2019-09-24 20:45:36 +0300

    rewrite consumers

--- backend/chat/services.py
+++ backend/chat/services.py
@@ -1,1 +1,11 @@
-def create_thread_with_one_user(creator, participant)
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
+
+def get_last_10_messages(chat_id):
+    chat = get_object_or_404(Chat, id=chat_id)
+    return Thread.messages.order_by('-timestamp').all()[:10]
+
+def get_current_chat(chat_id):
+    return get_object_or_404(Chat, id=chat_id)
+
+    
